352ce08a1b68b7d777924a36ddbf0cf05b7e4786
Marc0Antonio <162701191+Marc0Antonio@users.noreply.github.com>
2024-06-27 12:27:55 -0300

    Atualização

---
+++ assets/style.cs
@@ -0,0 +1,53 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+form {
+    width: 30%;
+    height: 30%;
+    margin: 20px auto;
+    padding: 20px;
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+form input[type="text"],
+form select,
+form input[type="checkbox"],
+form input[type="radio"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    display: block;
+}
+
+form input[type="submit"] {
+    background-color: #52753a;
+    color: white;
+    padding: 10px 200px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    text-align: center;
+    display: block;
+    margin: 0 auto;
+}
+
+form a[href="#"] {
+    color: green;
+}
+
+
+form gender[type="radio"] {
+    width: 10px;
+}
